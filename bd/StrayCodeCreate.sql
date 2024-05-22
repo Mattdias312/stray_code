172987c51d20ae637ce2c13a4baafc9e47c0ef1d
Prado <142859744+mdoisp@users.noreply.github.com>
2024-05-21 21:01:28 -0300

    Add files via upload
    Add inserts corretos atualizando tributação e nota fiscal

--- bd/StrayCodeCreate.sql
+++ bd/StrayCodeCreate.sql
@@ -1,3 +1,10 @@
+create table Tipo_Empresa(
+id_empresa int identity (1,1) not null,
+tipo_empresa varchar (30) not null,
+descricao varchar (2000) not null,
+Constraint PK_empresa Primary Key(id_empresa)
+);
+
 create table Nota_Fiscal(
 id_Nota_Fiscal int identity (1,1) not null,
 tipos_nota varchar (60) not null,
@@ -5,13 +12,6 @@ descricao varchar (2000) not null,
 tipo_empresa int not null,
 Constraint PK_Nota_fiscal Primary Key(id_Nota_Fiscal),
 Constraint FK_tipo_empresa_nota foreign Key(tipo_empresa) References tipo_empresa(id_empresa)
-);
-
-create table Tipo_Empresa(
-id_empresa int identity (1,1) not null,
-tipo_empresa varchar (30) not null,
-descricao varchar (2000) not null,
-Constraint PK_empresa Primary Key(id_empresa)
 );
 
 create table Ramo_Empresa(
@@ -86,7 +86,93 @@ Constraint FK_cnae_ass foreign Key(cnae) References Cnae(id_cnae),
 Constraint FK_tributacao_ass foreign Key(tributacao) References Tributacao(id_tributacao)
 );
 
+insert into Ramo_Empresa values
+('A',3,'AGRICULTURA, PECUÁRIA E SERVIÇOS RELACIONADOS'),
+('A',3,'PRODUÇÃO FLORESTAL'),
+('B',3,'EXTRAÇÃO DE CARVÃO MINERAL'),
+('B',3,'EXTRAÇÃO DE MINERAIS METÁLICOS'),
+('E',2,'CAPTAÇÃO, TRATAMENTO E DISTRIBUIÇÃO DE ÁGUA'),
+('E',2,'COLETA, TRATAMENTO E DISPOSIÇÃO DE RESÍDUOS; RECUPERAÇÃO DE MATERIAIS'),
+('G',1,'COMÉRCIO E REPARAÇÃO DE VEÍCULOS AUTOMOTORES E MOTOCICLETAS'),
+('G',1,'COMÉRCIO POR ATACADO, EXCETO VEÍCULOS AUTOMOTORES E MOTOCICLETAS'),
+('H',2,'TRANSPORTE TERRESTRE'),
+('H',2,'TRANSPORTE AQUAVIÁRIO');
+
+insert into Cnae values
+('0113-0/00','Cultivo de cana-de-açúcar',2),
+('0131-8/00','Cultivo de laranja',2),
+('0210-1/01','Cultivo de eucalipto',3),
+('0220-9/04','Coleta de látex em florestas nativas',3),
+('0500-3/01','Extração de carvão mineral',4),
+('0710-3/01','Extração de minério de ferro',5),
+('0721-9/01','Extração de minério de alumínio',5),
+('3600-6/01','Captação, tratamento e distribuição de água',6),
+('3600-6/02','Distribuição de água por caminhões',6),
+('3811-4/00','Coleta de resíduos não perigosos',7),
+('3900-5/00','Descontaminação e outros serviços de gestão de resíduos',7),
+('4511-1/01','Comércio a varejo de automóveis, camionetas e utilitários novos',8),
+('4512-9/01','Representantes comerciais e agentes do comércio de veículos automotores',8),
+('4611-7/00','Representantes comerciais e agentes do comércio de matérias-primas agrícolas e animais vivos',9),
+('4621-4/00','Comércio atacadista de café em grão',9),
+('4912-4/01','Transporte ferroviário de passageiros intermunicipal e interestadual',10),
+('4923-0/01','Serviço de táxi',10),
+('5011-4/02','Transporte marítimo de cabotagem - Passageiros',11),
+('5011-4/01','Transporte marítimo de cabotagem - Carga',11);
+
+insert into Tipo_Empresa values
+('Comércio','Empresas que compram e vendem produtos. Isso pode incluir varejistas, atacadistas e empresas de comércio eletrônico.'),
+('Serviços','Empresas que oferecem serviços em vez de produtos tangíveis. Isso pode incluir empresas de consultoria, de tecnologia da informação, de saúde, de educação, de transporte, entre outras.'),
+('Indústria','Empresas que fabricam produtos físicos. Isso pode incluir indústrias de manufatura, de construção, de energia, entre outras.');
+
+insert into Nota_Fiscal(tipos_nota, descricao, tipo_empresa)
+values ('NF-e (Nota Fiscal Eletrônica de Produtos ou Mercadorias)', 'Esse documento substitui o modelo tradicional. 
+É a versão digital, emitida e armazenada eletronicamente. Limita-se a registrar a venda de produtos físicos, 
+com cobrança de ICMS (Imposto sobre Circulação de Mercadoria Serviços) e IPI (Imposto sobre Produtos Industrializados) 
+e deve ser expedida junto às Secretarias Estaduais da Fazenda.
+Utilizada sempre em casos de circulação de mercadorias, geralmente, este tipo de nota fiscal é utilizado entre pessoas jurídicas. 
+Ao emitir a NF-e para o comprador, para atestar a operação, a empresa deve enviar um Documento Auxiliar da Nota Fiscal (DANFE) 
+com o produto.', 1),
+('NFS-e (Nota Fiscal Eletrônica de Serviços)', 'Esse é um documento fiscal digital necessário para confirmação de prestação de serviço 
+de uma instituição para outra ou para pessoa física. Sua emissão, na maioria das vezes, engloba pequenas e médias empresas. 
+Esse modelo de nota deve ser expedido junto à prefeitura em que está registrado o CNPJ da organização prestadora de serviço.
+A NFS-e se diferencia por necessitar apenas da Inscrição Municipal, que gera o DANFSE (Documento Auxiliar da Nota Fiscal de Serviços 
+Eletrônica), e tem como característica ser um modelo mais flexível. Esse modelo é usado para serviços de assinatura, Saas (oferta de 
+softwares e soluções na internet como um serviço), ou, ainda, para produtos digitais, como livros eletrônicos, cursos online, 
+entre outros. É emitida por qualquer operadora de serviço, como creches, hotéis, academias e afins.', 2);
+
+insert into Tributacao (tipos_tributacao, descricao)
+values ('MEI', 'O regime do MEI foi criado em 2008 para reduzir os níveis de informalidade, proporcionando aos empreendedores meios 
+mais acessíveis de regularização fiscal. Para tanto, o MEI se apresenta de forma menos burocrática, tendo regras diferenciadas para o 
+pagamento de impostos — IRPJ e CLSS, por exemplo, não entram no cálculo tributário dos microempreendedores individuais. Ainda sobre o pagamento de impostos, os valores de contribuição também são diferenciados no MEI: os cadastrados pagam mensalmente um valor de até R$ 60,60 — de acordo com a atividade exercida.
+Para que possam se enquadrar neste regime tributário, os empresários devem ter um faturamento bruto anual de até R$ 81 mil e não 
+possuir sócios. Atendendo a esses requisitos, eles podem contar com uma série de benefícios, inclusive previdenciários, como: 
+aposentadoria por idade, licença-maternidade, auxílio-doença, entre outros. '),
+('Simples Nacional', '- Reúne em um único documento, chamado de DAS (Documento de Arrecadação do Simples Nacional), 
+as 8 taxas que devem ser pagas;
+- Possui alíquotas menores que os demais regimes;
+- É menos burocrático.
+Para se enquadrar no Simples, é preciso ser microempreendedor individual (MEI), microempresa (ME) ou empresa de pequeno porte (EPP) e 
+cumprir os seguintes requisitos financeiros:
+1 - O MEI não pode ter faturamento anual acima de R$ 81 mil;
+2 - Para ME, o teto é de R$ 360 mil por ano;
+3 - O limite anual para a EPP é R$ 4,8 milhões.'),
+('Lucro Real', 'O enquadramento ao Lucro Real é obrigatório nos casos de bancos e corretoras de título e, ainda, caso a empresa tenha 
+tido uma receita bruta superior a R$ 78 milhões no ano anterior.
+Diferentemente do Simples Nacional, os tributos são cobrados individualmente, o que requer um maior cuidado das empresas para que 
+algum não fique em aberto. Como o cálculo de cada taxa é baseado no lucro, esse regime é indicado para empresas que não tenham uma 
+margem de lucro muito elevada, atuem no prejuízo ou, então, tenham custos elevados em serviços ou produtos.'),
+('Lucro Presumido', 'Neste tipo de tributação, a incidência de impostos é baseada na estimativa de lucro da empresa e calculada 
+conforme a atividade que é desempenhada. No caso, os impostos podem variar de 8% (indústria e comércio) a 32% (serviço).
+Qualquer negócio que não é obrigado a aderir ao Lucro Real pode optar por esse regime, porém ele é mais vantajoso para as empresas que:
+- Apresentarem margem de lucro superior ao da presunção;
+- Tenham baixos custos operacionais;
+- Possuam folha de pagamento enxuta;
+- Não ultrapassem o faturamento de R$ 78 milhões.
+- Existe, porém, uma desvantagem que deve ser levada em consideração. Caso a empresa lucre menos do que a média das demais empresas 
+que exercem a mesma atividade, ela deverá pagar um imposto em cima da alíquota pré-fixada (8%).');
+
 select * from Tipo_Empresa
 select * from Ramo_Empresa
 select * from Cnae
+select * from Nota_Fiscal
 
